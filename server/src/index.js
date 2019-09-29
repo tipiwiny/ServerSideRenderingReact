f1f3725cf2f1d3be09ac3675fea414544aa06676
Juan Luis Perez <tipiwiny@gmail.com>
2019-09-29 19:54:10 +0200

    creating header

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,33 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
 import proxy from 'express-http-proxy';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
-import {matchRoutes} from 'react-router-config';
-import Routes from './client/Routes';
 
 const app = express();
 
-app.use('/api', proxy('http://react-ssr.api.herokuapp.com',
-{
-  proxyReqOptDecorators(opts) {
-    opts.headers['x-forwarded-host'] = 'localhost:3000'
-    return opts
-  }
-}));
-app.use(express.static('public'));
-app.get('*', async (req, res) => {
-  const store = createStore(req)
-
-  const promises = matchRoutes(Routes, req.path).map(({route}) => {
-    return route.loadData ? route.loadData(store) : null
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    }
   })
-  await Promise.all(promises)
-  res.send(renderer(req,store));
+);
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+  const store = createStore(req);
+
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
