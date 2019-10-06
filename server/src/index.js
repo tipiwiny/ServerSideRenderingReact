17730929be87af6eb584896eb021b1a900c1e837
Juan Luis Perez <tipiwiny@gmail.com>
2019-10-06 13:37:58 +0200

    auth requirement

--- server/src/index.js
+++ server/src/index.js
@@ -21,15 +21,30 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore(req);
 
-  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(Routes, req.path)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null;
+    })
+    .map(promise => {
+      if (promise) {
+        return new Promise((resolve, reject) => {
+          promise.then(resolve).catch(resolve);
+        });
+      }
+    });
+
   Promise.all(promises).then(() => {
-    const context = {}
-    const content = renderer(req, store, context)
-    if(context.notFound) {
-      res.status(404)
+    const context = {};
+    const content = renderer(req, store, context);
+    console.log('context', context)
+    if (context.url) {
+      console.log('entra')
+      return res.redirect(301, context.url);
     }
+    if (context.notFound) {
+      res.status(404);
+    }
+
     res.send(content);
   });
 });
