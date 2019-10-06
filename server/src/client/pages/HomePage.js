5c9e76532b1e57adee63f5c6c304a0325afc0a93
Juan Luis Perez <tipiwiny@gmail.com>
2019-10-06 11:56:20 +0200

    auth reducer and styling app

--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <div>I'm the VERY VERY BEST home component</div>
-      <button onClick={() => console.log('Hi there!')}>Press me!</button>
+    <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
     </div>
   );
 };
+
 export default {
-  component:Home}
-  ;
+  component: Home
+};
--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -16,7 +16,9 @@ const content = renderToString(
 
 return `
   <html>
-    <head></head>
+    <head>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+     </head>
     <body>
       <div id="root">${content}</div>
       <script>
